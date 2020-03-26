98290c46134d140157ff827265f41fd46edd5b7a
brckhan88 <brckhan88@gmail.com>
2020-03-26 13:42:30 +0300

    Update controllers.js

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -5,6 +5,8 @@ angular.module('starter.controllers', [])
     $rootScope.webServiceUrl = "http://www.microwebservice.net/operics_web/webservice.php";
     $scope.pictureUrl = "http://placehold.it/200x200";
 
+    localStorage.setItem('language', "EN");
+
     $scope.language = localStorage.getItem('language');
 
     if (!$scope.language) {
